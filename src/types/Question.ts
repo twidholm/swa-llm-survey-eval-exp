0607bf18e6268fe137408457e2a77d25b89aed64
Tim Widholm <widholmtim@gmail.com>
2024-12-08 15:29:34 +0100

    Change question object formatting

--- src/types/Question.ts
+++ src/types/Question.ts
@@ -1,5 +1,9 @@
 export type Question = {
   id: string
   text: string
-  answer_options: Array<string>
+  answer_options: Array<AnswerOption>
+}
+export type AnswerOption = {
+  code: number
+  optionText: string
 }
